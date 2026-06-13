f8b4e7284a90078511b4c95fb4785d240b45a052
SuPeR <allandev472@gmail.com>
2026-06-13 12:08:54 -0300

    Questão A

--- Lista 2 VJudge/A.cpp
+++ Lista 2 VJudge/A.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
 
+template<typename E>
+class Dictionary{
+private:
+    enum Estado{VAZIO, OCUPADO, DELETADO};
+
+    static const int tam = 101;
+    E values[tam];
+    Estado estado[tam];
+    int cont;
+
+public:
+    Dictionary() : cont(0) {
+        for(int i = 0, i < tam; i++){
+            estado[i] = VAZIO;
+        }
+    }
+    ~Dictionary();
+
+    void insert(string key, E value);
+    E remove(string key);
+    E find(string key);
+    void clear();
+    int size();
+    
+};
 
 int main(){
     return 0;
